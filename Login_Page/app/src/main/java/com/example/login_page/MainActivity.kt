dfb77c4d7e5d66b7e26d66f1f39dda6550d7336d
roy_hendel <46100559+royhendel@users.noreply.github.com>
2020-01-11 17:27:27 +0200

    currently not working, login stage

--- Login_Page/app/src/main/java/com/example/login_page/MainActivity.kt
+++ Login_Page/app/src/main/java/com/example/login_page/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,10 +20,20 @@ class MainActivity : AppCompatActivity() {
         var Password_card = findViewById(R.id.Password) as TextView
         var Username = findViewById(R.id.Username) as TextView
 
-        var LoginInfoDatabase =
+        var db = FirebaseFirestore.getInstance()
+        val San1Data = hashMapOf(
+            "UID" to "uid",
+            "password" to "plz?",
+            "Role" to "Saniter"
+        )
+
+        db.document("San1").set(San1Data)
+        val users = db.collection("users")
+        val query = users.whereEqualTo("UID", "uid")
 
         Login_Button.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Verifying User info", Toast.LENGTH_LONG).show()
+            Toast.makeText(this@MainActivity,"query.toString()", Toast.LENGTH_LONG).show()
+
         }
 
     }
